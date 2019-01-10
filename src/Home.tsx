61b6bbf4dde58e8cae0d773247360322d2ed585a
Sungwoo <sungwoo.cho.dev@gmail.com>
2019-01-10 10:57:29 +0900

    Server Debug & SCSS Plugin

--- src/Home.tsx
+++ src/Home.tsx
@@ -2,7 +2,7 @@ import { AfterRenderProps } from '@jaredpalmer/after';
 import React, { Component } from 'react';
 import { RouteChildrenProps } from 'react-router';
 import { Link } from 'react-router-dom';
-import './Home.css';
+import './Home.scss';
 import logo from './react.svg';
 
 type InitialProps = AfterRenderProps<{}> & RouteChildrenProps;
